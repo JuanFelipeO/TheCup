09e55a886ad38e581b7ee1c908e6ee1099737f5e
JUAN <jfortiz87911@umanizales.edu.co>
2021-10-20 21:18:54 -0500

    El proyecto se crea con las caracteristicas basicas para el funcionamineto basico del mismo

---
+++ src/test/java/com/umanizales/thecup/TheCupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umanizales.thecup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
